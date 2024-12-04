674715a8d75246464ffecbb3a4b1cad011820bfb
omogbolahan94 <gabrielomogbolahan1@gmail.com>
2024-12-04 08:36:11 +0100

    set packages and created all necessary files

---
+++ vector_store.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import os
+
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone, ServerlessSpec
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+# from langchain.embeddings.openai import OpenAIEmbeddings
+
+load_dotenv()
+
+if __name__ == '__main__':
+    secret_key = os.getenv('PINECONE_API_TOKEN')
+
+    # set up an exisiting vector store with the index name 'finance'
+    pc = Pinecone(api_key=pinecone_key)
+    index = pc.Index('finance')
+
+    # load the dataset
+    loader = DirectoryLoader('/content/drive/My Drive/Finance_Investment_LLM/Data', glob="./*.docx")
+    data = loader.load()
+
+    print(secret_key)
